183f034b87fc1153cfe36352eefe7f1e8841fd69
Ben Hayes <b.t.hayes@comcast.net>
2021-08-31 18:06:28 -0500

    commit tree 5b6f4b52aa64c61ae871d7142cb438251677edc5

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,9 +2,19 @@ import React from 'react';
 import { actors } from '../data';
 
 const Actors = () => {
+
+  const actorDivs = actors.map(actor => {
+    return <div>
+      <h3>Name: {actor.name}</h3>
+      Movies: <ul>{actor.movies.map(movie => <li>{movie}</li>)}</ul>
+    </div>
+  })
+
+
   return (
     <div>
-      {/*{code here}*/}
+      <h1>Actors Page</h1>
+      {actorDivs}
     </div>
   );
 };
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,9 +2,21 @@ import React from 'react';
 import { movies } from '../data';
 
 const Movies = () => {
+
+  const movieDivs = movies.map(movie => {
+    return <div>
+      <h3>Name: {movie.title}</h3>
+      Time: {movie.time}
+      <br/>
+      <br/>
+      Genres: <ul>{movie.genres.map(genre => <li>{genre}</li>)}</ul>
+    </div>
+  })
+
   return (
     <div>
-        {/*{code here}*/}
+      <h1>Movies Page</h1>
+      {movieDivs}
     </div>
   );
 };
